0064e9b56fa20676f6cb53b52a1d098eb85aa858
minfeng <missiontoolshare@gmail.com>
2026-01-17 01:03:12 +0800

    修改+上傳

---
+++ Employee Calculator - EP1/Employee Calculator/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Employee_Calculator
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int BaseSalary { get; set; }
+        public int Bonus { get; set; }
+
+        public int TotalSalary
+        {
+            get { return BaseSalary + Bonus; }
+        }
+    }
+}
